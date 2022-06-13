611053a099419c0a3599840c71e3f1e199659002
Esfekia <sertac@gmail.com>
2022-06-13 06:36:30 -0400

    Player's idle and moving animations, as well as turning to the side where it is going, are added and work correctly.
    Player's idle and moving animations, as well as turning to the side where it is going, are added and work correctly.

--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -7,12 +7,15 @@ public class PlatformerPlayer : MonoBehaviour
     public float speed = 4.5f;
 
     private Rigidbody2D body;
+
+    private Animator anim;
         
     // Start is called before the first frame update
     void Start()
     {
         // need this other component attached to this GameObject
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,5 +25,15 @@ public class PlatformerPlayer : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * speed;
         Vector2 movement = new Vector2(deltaX, body.velocity.y);
         body.velocity = movement;
+
+        // speed is greater than zero even if velocity is negative
+        anim.SetFloat("speed", Mathf.Abs(deltaX));
+
+        // floats aren't always exact, so compare using Approximately()
+        if (!Mathf.Approximately(deltaX, 0))
+        {
+            // when moving, scale positive or negative 1 to face right or left
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+        }
     }
 }
